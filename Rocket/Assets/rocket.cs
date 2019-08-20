b4872ab65a309b95146e6944b9640153303e947e
sadfa <sadfa@hotmail.com>
2019-08-20 11:32:29 -0400

    version fonctionnel 1

---
+++ Rocket/Assets/rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+
+    Rigidbody rbd;
+    public int SPEEDFACTOR = 1000;
+    public int ORIGINE = 1;
+
+    // Use this for initialization
+    void Start () {
+        rbd = gameObject.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Jump")!=0)
+        {
+            print("space key was pressed");
+            rbd.AddRelativeForce(new Vector3(0, Input.GetAxis("Jump") * Time.deltaTime * SPEEDFACTOR), 0);
+
+        }
+
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            rbd.AddForceAtPosition(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * SPEEDFACTOR, 0, 0),new Vector3(0, ORIGINE, 0));
+
+        }
+    }
+}
